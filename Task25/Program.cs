3d19786295b8fe3c040e164ab518eb392f115bd2
Ekaterina <ekaterina266226@mail.ru>
2022-08-22 12:49:08 +0300

    Add gitignore

---
+++ Task25/Program.cs
@@ -0,0 +1,15 @@
+﻿// 1Напишите цикл, который принемает на вход два числа(А и B)
+// 2Возводит число А в натуральную степень B
+
+Console.Write("Введите певое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int work = 1;
+while (i < num2)
+{
+    work = num1 * work;
+    i++;
+}
+Console.WriteLine($"{num1} в степени {num2} равна: {work}");
